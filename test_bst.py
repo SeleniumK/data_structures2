d3c46f7df0d1becda6b1cfc612ecd557578984c7
Norton Pengra <npengra317@gmail.com>
2016-04-07 17:42:43 -0700

    Built tests for Traversals and Fixture

--- test_bst.py
+++ test_bst.py
@@ -3,6 +3,19 @@ from bst import Node, Bst
 import random
 
 
+@pytest.fixture
+def bst_root_fifty():
+    test_tree = Bst()
+    test_tree.insert(50)
+    test_tree.insert(25)
+    test_tree.insert(100)
+    test_tree.insert(12)
+    test_tree.insert(35)
+    test_tree.insert(75)
+    test_tree.insert(150)
+    return test_tree
+
+
 def test_create_node_default():
     """Assert that you can create a node with no children."""
     test_node = Node()
@@ -29,7 +42,7 @@ def test_add_left_child2():
 
 
 def test_add_left_child_expect_error():
-    """Assert that you cannot assign a child to a node when that sided child 
+    """Assert that you cannot assign a child to a node when that sided child
     already exists.
     """
     child_node = Node()
@@ -54,7 +67,7 @@ def test_add_right_child2():
 
 
 def test_add_right_child_expect_error():
-    """Assert that you cannot assign a child to a node when that sided child 
+    """Assert that you cannot assign a child to a node when that sided child
     already exists.
     """
     child_node = Node()
@@ -210,14 +223,18 @@ def test_depth():
     assert test_tree.depth() == 8
 
 
+def test_bst_fixture(bst_root_fifty):
+    assert bst_root_fifty.root.value == 50
+    assert bst_root_fifty.depth() == 3
 
 
+def test_in_order(bst_root_fifty):
+    assert bst_root_fifty.in_order() == [12, 25, 35, 50, 75, 100, 150]
 
 
+def test_pre_order(bst_root_fifty):
+    assert bst_root_fifty.pre_order() == [50, 25, 12, 35, 100, 75, 150]
 
 
-
-
-
-
-
+def test_post_order(bst_root_fifty):
+    assert bst_root_fifty.post_order() == [12, 35, 25, 75, 150, 100, 50]
