a3cde35a13664c71ba8f78e3131b959f484e87b3
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-04-11 06:41:05 -0700

    Some delete work

--- test_bst.py
+++ test_bst.py
@@ -17,6 +17,15 @@ def bst_root_fifty():
 
 
 @pytest.fixture
+def three_bst():
+    test_tree = Bst()
+    test_tree.insert(50)
+    test_tree.insert(25)
+    test_tree.insert(100)
+    return test_tree
+
+
+@pytest.fixture
 def empty_bst():
     return Bst()
 
@@ -46,14 +55,14 @@ def test_add_left_child2():
     assert test_node.left_child is child_node
 
 
-def test_add_left_child_expect_error():
-    """Assert that you cannot assign a child to a node when that sided child
-    already exists.
-    """
-    child_node = Node()
-    test_node = Node(left_child=child_node)
-    with pytest.raises(TypeError):
-        test_node.left_child = child_node
+# def test_add_left_child_expect_error():
+#     """Assert that you cannot assign a child to a node when that sided child
+#     already exists.
+#     """
+#     child_node = Node()
+#     test_node = Node(left_child=child_node)
+#     with pytest.raises(TypeError):
+#         test_node.left_child = child_node
 
 
 def test_add_right_child():
@@ -71,14 +80,14 @@ def test_add_right_child2():
     assert test_node.right_child is child_node
 
 
-def test_add_right_child_expect_error():
-    """Assert that you cannot assign a child to a node when that sided child
-    already exists.
-    """
-    child_node = Node()
-    test_node = Node(right_child=child_node)
-    with pytest.raises(TypeError):
-        test_node.right_child = child_node
+# def test_add_right_child_expect_error():
+#     """Assert that you cannot assign a child to a node when that sided child
+#     already exists.
+#     """
+#     child_node = Node()
+#     test_node = Node(right_child=child_node)
+#     with pytest.raises(TypeError):
+#         test_node.right_child = child_node
 
 
 def test_insert_nonnumber():
@@ -107,7 +116,15 @@ def test_insert_right_child_tree():
     """Assert that insert right child works as expected."""
     test_tree = Bst()
     test_tree.insert(1)
+    assert test_tree.root.value == 1
+    assert test_tree.root.left_child is None
+    assert test_tree.root.right_child is None
+    assert test_tree.root.parent is None
     test_tree.insert(0)
+    assert test_tree.root.left_child.value == 0
+    assert test_tree.root.left_child.parent.value == 1
+    assert test_tree.root.right_child is None
+    assert test_tree.root.right_child is None
     test_tree.insert(4)
     assert test_tree.root.right_child.value == 4
     test_tree.insert(7)
@@ -154,6 +171,7 @@ def test_contains_is_false():
     test_tree.insert(3)
     test_tree.insert(1)
     assert not test_tree.contains(7)
+
 
 def test_contains_is_false2():
     """Assert that tree does not contain value."""
@@ -266,3 +284,132 @@ def test_empty_tree_traversals(empty_bst):
     assert list(empty_bst.post_order()) == []
     assert list(empty_bst.in_order()) == []
     assert list(empty_bst.breadth_first()) == []
+
+
+def test_delete_empty(empty_bst):
+    empty_bst.delete(7)
+    assert empty_bst.root is None
+
+
+def test_delete_with_no_children(empty_bst):
+    empty_bst.insert(7)
+    assert empty_bst.root.value == 7
+    empty_bst.delete(7)
+    assert empty_bst.root is None
+
+
+def test_delete_bottom_rung(bst_root_fifty):
+    assert bst_root_fifty.depth() == 3
+    for num in [12, 35, 75, 150]:
+        bst_root_fifty.delete(num)
+        assert not bst_root_fifty.contains(num)
+    assert bst_root_fifty.depth() == 2
+    for num in [25, 100]:
+        assert bst_root_fifty._contains(num).left_child is None
+        assert bst_root_fifty._contains(num).right_child is None
+
+
+def test_delete_middle(bst_root_fifty):
+    bst_root_fifty.delete(25)
+    # import pdb; pdb.set_trace()
+    assert bst_root_fifty.root.value == 50
+    assert bst_root_fifty.root.left_child.value == 35 
+    assert bst_root_fifty.root.left_child.left_child is None
+    assert bst_root_fifty.root.left_child.right_child.value == 35
+    assert bst_root_fifty.root.left_child.right_child.parent.value == 12
+    assert not bst_root_fifty.contains(25)
+
+
+def test_delete_root(bst_root_fifty):
+    bst_root_fifty.delete(50)
+    root = bst_root_fifty.root
+    assert root.value == 25
+    assert root.right_child.value == 100
+    assert root.left_child.value == 12
+    assert root.right_child.right_child.value == 35
+    assert root.right_child.right_child.value == 150
+    assert root.right_child.left_child.value == 75
+# def test_delete_node_both_children(bst_root_fifty):
+#     bst_root_fifty.delete(100)
+#     assert not bst_root_fifty.contains(100)
+#     assert bst_root_fifty.root.right_child.left_child.value == 75
+#     assert bst_root_fifty.root.right_child.value == 150
+
+
+# def test_delete_node_both_children_root(bst_root_fifty):
+#     bst_root_fifty.delete(50)
+#     assert bst_root_fifty.root.value == 100
+
+
+# def test_delete_parent(bst_root_fifty):
+#     bst_root_fifty.insert(30)
+#     bst_root_fifty.insert(37)
+#     bst_root_fifty.delete(35)
+#     root_grandchild = bst_root_fifty.root.left_child.right_child
+#     assert root_grandchild.value == 37
+#     assert root_grandchild.left_child.value == 30
+
+
+# def test_delete_grampa(bst_root_fifty):
+#     bst_root_fifty.insert(6)
+#     bst_root_fifty.insert(13)
+#     bst_root_fifty.delete(25)
+#     root_left_child = bst_root_fifty.root.left_child
+#     assert root_left_child.value == 35
+#     assert root_left_child.left_child.value == 12
+
+
+# def test_delete_parent_one_child(bst_root_fifty):
+#     bst_root_fifty.insert(6)
+#     bst_root_fifty.delete(12)
+#     root_left_child = bst_root_fifty.root.left_child
+#     assert root_left_child.value == 25
+#     assert root_left_child.left_child.value == 6
+
+
+# def test_delete_parent_one_child_right(bst_root_fifty):
+#     bst_root_fifty.insert(27)
+#     bst_root_fifty.delete(35)
+#     root_left_child = bst_root_fifty.root.left_child
+#     assert root_left_child.value == 25
+#     assert root_left_child.right_child.value == 27
+
+
+# def test_delete_parent_two_children(bst_root_fifty):
+#     bst_root_fifty.insert(6)
+#     bst_root_fifty.insert(13)
+#     bst_root_fifty.delete(12)
+#     root_left_child = bst_root_fifty.root.left_child
+#     assert root_left_child.value == 25
+#     assert root_left_child.left_child.value == 13
+#     assert root_left_child.left_child.left_child.value == 6
+
+
+# def test_two_node_bst(empty_bst):
+#     empty_bst.insert(100)
+#     empty_bst.insert(75)
+#     empty_bst.insert(150)
+#     empty_bst.delete(100)
+#     assert empty_bst.root.value == 150
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
