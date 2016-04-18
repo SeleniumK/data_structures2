056aba1082227bd815babe0ee13dd36918c2a5af
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-04-18 15:46:30 -0700

    ugh

--- src/insertion.py
+++ src/insertion.py
@@ -1,12 +1,17 @@
+from itertools import permutations
+
 def insertion_sort(lst):
-    # import pdb; pdb.set_trace()
+    new_lst = lst[:]
     for i, node in enumerate(lst):
         before = i - 1
+        current = i
         while before >= 0:
-            if lst[i] < lst[before]:
-                lst[before], lst[i] = lst[i], lst[before]
+            if new_lst[current] < new_lst[before]:
+                new_lst[before], new_lst[current] = new_lst[current], new_lst[before]
             before -= 1
-    return lst
+            current -= 1
+    return new_lst
+
 
 if __name__ == "__main__":
-    pass
+    
--- tests/test_insertion.py
+++ tests/test_insertion.py
@@ -3,9 +3,21 @@ from itertools import permutations
 from src.insertion import insertion_sort
 
 
-LIST_THREE = list(map(list, permutations(range(3))))
+THREE = list(map(list, permutations(range(3))))
+FOUR = list(map(list, permutations(range(4))))
+FIVE = list(map(list, permutations(range(5))))
 
 
-@pytest.mark.parametrize("lst", LIST_THREE)
+@pytest.mark.parametrize("lst", THREE)
 def test_insertion(lst):
     assert insertion_sort(lst) == [0, 1, 2]
+
+
+@pytest.mark.parametrize("lst", FOUR)
+def test_insertion(lst):
+    assert insertion_sort(lst) == [0, 1, 2, 3]
+
+
+@pytest.mark.parametrize("lst", FIVE)
+def test_insertion(lst):
+    assert insertion_sort(lst) == [0, 1, 2, 3, 4]
