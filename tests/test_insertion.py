7eb0dc733e26f5f75a55b5b52c90e895c39f72af
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-04-20 16:26:29 -0700

    fixed tests

--- tests/test_insertion.py
+++ tests/test_insertion.py
@@ -2,22 +2,15 @@ import pytest
 from itertools import permutations
 from src.insertion import insertion_sort
 
+TO_SORT = (
+    [[]] +
+    [[1]] +
+    list(map(list, permutations(range(3)))) +
+    list(map(list, permutations(range(4)))) +
+    list(map(list, permutations(range(5))))
+)
 
-THREE = list(map(list, permutations(range(3))))
-FOUR = list(map(list, permutations(range(4))))
-FIVE = list(map(list, permutations(range(5))))
 
-
-@pytest.mark.parametrize("lst", THREE)
+@pytest.mark.parametrize("lst", TO_SORT)
 def test_insertion(lst):
-    assert insertion_sort(lst) == [0, 1, 2]
-
-
-@pytest.mark.parametrize("lst", FOUR)
-def test_insertion(lst):
-    assert insertion_sort(lst) == [0, 1, 2, 3]
-
-
-@pytest.mark.parametrize("lst", FIVE)
-def test_insertion(lst):
-    assert insertion_sort(lst) == [0, 1, 2, 3, 4]
+    assert insertion_sort(lst) == sorted(lst)
