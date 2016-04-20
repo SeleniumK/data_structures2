5c3cd0df29a86ffed76f349ef6b36b4182d3a5a0
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-04-20 16:17:11 -0700

    both mergesort and quicksort

--- src/mergesort.py
+++ src/mergesort.py
@@ -1,6 +1,5 @@
 def merge_sort(lst):
     """Sort list using the mergesort method."""
-
     half = len(lst) // 2
     left_side = lst[:half]
     right_side = lst[half:]
@@ -23,3 +22,29 @@ def merge_sort(lst):
             while side:
                 lst[i] = side.pop(0)
                 i += 1
+
+if __name__ == "__main__":
+    import timeit
+    from random import randint
+    times = 500
+
+    def test1():
+        merge_sort([2, 1])
+
+    def test2():
+        merge_sort([randint(0, 1000000) for i in range(1000)])
+
+    avg_time = timeit.Timer(test1).timeit(times)
+    avg_time_big = timeit.Timer(test2).timeit(times)
+
+    print("""Merge sort recursively splits the list in half and creates 
+a new list by comparing items from left to right in each of those lists.""")
+    print()
+
+    print("Input: [2, 1]")
+    print("\tnumber of runs: {}".format(times))
+    print("\taverage time  : {}".format(avg_time))
+    print()
+    print("Input: [randint(0, 1000000) for i in range(1000)]")
+    print("\tnumber of runs: {}".format(times))
+    print("\taverage time  : {}".format(avg_time_big))
